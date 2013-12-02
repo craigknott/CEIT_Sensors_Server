f1e3cb832745ec4882545f408feb6fda801a1e5b
Craig Knott <craig.knott92@gmail.com>
2013-12-02 10:14:32 +1000

    Update publish3d.py

--- publish3d.py
+++ publish3d.py
@@ -13,48 +13,51 @@ import mosquitto
 import json
 import random
 
-mqttc = None
-            
-def on_connect(mosq, obj, rc):
-    if (rc == 0):
-        print("Connected successfully")
 
-def on_publish(mosq, obj, mid):
-    print("Message " + str(mid) + " published")
+class pub3d():            
+    def on_connect(self, mosq, obj, rc):
+        if (rc == 0):
+            print("Connected successfully")
     
-def on_message(mosq, obj, msg):
-    print("Message received on topic "+msg.topic+" with QoS "+str(msg.qos)+" and payload "+msg.payload)
-    if msg.payload is not None:
-        data = str(msg.payload)
-        try:
-            data2 = json.loads(data)
-        except ValueError:
-            print("Value Error loading data")
-            return
+    def on_publish(self, mosq, obj, mid):
+        print("Message " + str(mid) + " published")
         
-        datastream = data2['id']
-        value = data2['value']
-        print datastream
-        print value
-        MQTT.packet['id'] = datastream
-        MQTT.packet['value'] = str(value)
-        data = json.dumps(MQTT.packet)
-        mqttc.publish(MQTT.topic_3d, data)
-
-def on_subscribe(mosq, obj, mid, qos_list):
-    print("Subscribe with mid "+str(mid)+" received.")
+    def on_message(self, mosq, obj, msg):
+        print("Message received on topic "+msg.topic+" with QoS "+str(msg.qos)+" and payload "+msg.payload)
+        if msg.payload is not None:
+            data = str(msg.payload)
+            try:
+                data2 = json.loads(data)
+            except ValueError:
+                print("Value Error loading data")
+                return
+            
+            datastream = data2['id']
+            value = data2['value']
+            print datastream
+            print value
+            MQTT.packet['id'] = datastream
+            MQTT.packet['value'] = str(value)
+            data = json.dumps(MQTT.packet)
+            self.mqttc.publish(MQTT.topic_3d, data)
     
-
-def start_mosquitto(server, client_id, topic, username = None, password = None):
-    global mqttc = mosquitto.Mosquitto(client_id)
-    mqttc.connect(server, 1883, 60, True)
-    mqttc.subscribe(MQTT.topic_temp)
-    mqttc.on_connect = on_connect
-    mqttc.on_subscribe = on_subscribe
-    mqttc.on_message = on_message
-    mqttc.on_publish = on_publish
-    mqttc.loop();
+    def on_subscribe(self, mosq, obj, mid, qos_list):
+        print("Subscribe with mid "+str(mid)+" received.")
+        
     
+    def start_mosquitto(self, server, client_id, topic, username = None, password = None):
+        self.mqttc = mosquitto.Mosquitto(client_id)
+        self.mqttc.connect(server, 1883, 60, True)
+        self.mqttc.subscribe(MQTT.topic_temp)
+        self.mqttc.on_connect = on_connect
+        self.mqttc.on_subscribe = on_subscribe
+        self.mqttc.on_message = on_message
+        self.mqttc.on_publish = on_publish
+        self.mqttc.loop_start();
+        
+    def __init__(self):
+        start_mosquitto(MQTT.server, MQTT.client_3d, MQTT.topic_temp)
+        
 if __name__ == '__main__':
-    start_mosquitto(MQTT.server, MQTT.client_3d, MQTT.topic_temp)
-    
+    pub3d().__init__()
+
